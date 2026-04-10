d18afbbd371e7fcd1142a6bcf9e8e18f99afbe77
anshul <anshulgite2000@gmail.com>
2026-04-10 11:27:06 +0530

    feat: add role Hierarchy config
    Signed-off-by: anshul <anshulgite2000@gmail.com>

--- src/main/java/com/jwtAuthentication/security/SecurityConfig.java
+++ src/main/java/com/jwtAuthentication/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.jwtAuthentication.auth.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,5 +46,12 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    static RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withDefaultRolePrefix()
+                .role("ADMIN").implies("USER") // ADMIN के पास USER की सभी ताकत होगी
+                .build();
     }
 }
